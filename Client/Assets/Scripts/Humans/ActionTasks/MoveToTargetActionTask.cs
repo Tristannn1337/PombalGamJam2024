b517ae5f9323169dfef62fc068380c4b017c2611
Nuno Santos <n.m.santos8@gmail.com>
2024-10-12 16:15:38 +0100

    Implemented patrol behavior on mall cop.

--- Client/Assets/Scripts/Humans/ActionTasks/MoveToTargetActionTask.cs
+++ Client/Assets/Scripts/Humans/ActionTasks/MoveToTargetActionTask.cs
@@ -10,18 +10,35 @@ using UnityEngine;
 [Description("Moves the Agent towards a target using Pathfinding")]
 public class MoveToTargetActionTask : ActionTask<Human>
 {
-    protected override string info => $"Move to {(agent.Target != null ? agent.Target : "Target")}";
+    public BBParameter<Transform> Target;
+    public BBParameter<float> MovementSpeed;
+    public BBParameter<float> PathUpdateTime;
+    protected override string info => $"Move to {(Target.value != null ? Target.value : "Target")}";
+
+    float updateTimer;
+    protected override void OnExecute()
+    {
+        agent.Pathfinding.CalculatePath(false, Target.value.position);
+    }
 
     protected override void OnUpdate()
     {
+        updateTimer += Time.deltaTime;
+        if (updateTimer >= PathUpdateTime.value)
+        {
+            agent.Pathfinding.CalculatePath(false, Target.value.position);
+            updateTimer = 0f;
+        }
+
+
         if (!agent.Pathfinding.PathExists()) return;
 
         Vector2? nextWaypoint = agent.Pathfinding.GetNextWaypoint();
         if(nextWaypoint == null) return;
 
-        agent.transform.position = Vector2.MoveTowards(agent.transform.position, (Vector2)nextWaypoint, agent.MovementSpeed * Time.deltaTime);
+        agent.transform.position = Vector2.MoveTowards(agent.transform.position, (Vector2)nextWaypoint, MovementSpeed.value * Time.deltaTime);
 
-        if (agent.Pathfinding.ReachedDestination(agent.Target.position))
+        if (agent.Pathfinding.ReachedDestination(Target.value.position))
         {
             EndAction(true);
         }
