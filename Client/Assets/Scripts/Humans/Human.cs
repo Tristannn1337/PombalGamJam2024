c1a9f07d932c71622de38c23b63314b5174a88bc
Nuno Santos <n.m.santos8@gmail.com>
2024-10-12 16:24:09 +0100

    Started working on chase behavior.

--- Client/Assets/Scripts/Humans/Human.cs
+++ Client/Assets/Scripts/Humans/Human.cs
@@ -1,6 +1,13 @@
+using Pombal;
 using UnityEngine;
 
 public class Human : MonoBehaviour
 {
+    [field: SerializeField] public FishController Fish { get; private set; }
     [field: SerializeField] public AIPathfinding Pathfinding { get; private set; }
+
+    private void Awake()
+    {
+        Fish = FindObjectOfType<FishController>();
+    }
 }
